3311a45ccef9d680de204a3f30ce16be177a85c9
salftlow <2017320580274@whu.edu.cn>
2019-12-04 10:40:15 +0800

    Merge all text into one

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import os
 import glob
+from splitandocr import remove_dir
 
 path = "./pictures/*.jpg"
 
+dirname = "allresult"
+remove_dir(dirname)
+os.mkdir(dirname)
+
 files = glob.glob(path)
+count = 0
+for file in files:
+    os.system("python3 ./splitandocr.py "+file)
+    os.system("python3 ./splitandocr.py "+file+" --ocr")
+    sentence = ""
+    article = []
+    with open("results.txt","r") as f:
+        lines = f.readlines()
+        for line in lines:
+            sentence += line[0:-1]
+            if(line.find('。') != -1):
+                article.append(sentence)
+                sentence = ""
+
+    with open(os.path.join(dirname,"results.txt"),"a") as f:
+        for sentence in article:
+            f.write(sentence+"\n")
